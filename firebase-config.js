e5fa998a060945d0582e21419c5cdf966403d2d7
masterpg <masterpg@livedeveloper.net>
2017-09-10 11:10:05 +0900

    firebase-config.jsのテンプレートを修正

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-const config = {
+firebase.initializeApp({
   apiKey: "<API_KEY>",
   authDomain: "<PROJECT_ID>.firebaseapp.com",
   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
   projectId: "<PROJECT_ID>",
   storageBucket: "<BUCKET>.appspot.com",
   messagingSenderId: "<SENDER_ID>",
-};
-firebase.initializeApp(config);
+});
